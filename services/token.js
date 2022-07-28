a5c6cbc83a483f4f864157c76f21ef2b5e089f86
Hector Betancourt <hhbetancourt92@gmail.com>
2022-07-28 14:43:05 -0500

    token

--- services/token.js
+++ services/token.js
@@ -4,8 +4,8 @@ require('dotenv').config({path:"./.env"});
 function createToken(user){
     const payload = {
         _id:user._id,
-        Nombre:usuario.Nombre,
-        tipo_usuario:usuario.tipo_usuario
+        Uid:user.Uid,
+        Email:user.tipo_usuario
     }
     const token= process.env.CLAVE;
     return jwt.sign(payload, token)
