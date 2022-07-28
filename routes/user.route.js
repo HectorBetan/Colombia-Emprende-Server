74251171c4ce0ad468d3eef6de63fbc023bac780
Hector Betancourt <hhbetancourt92@gmail.com>
2022-07-28 12:45:45 -0500

    ch

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,9 +4,7 @@ router = express.Router();
 let userSchema = require('../models/Users');
 router.route('/create-user').post((req, res, next) => {
     console.log('reqbody',req.body);
-    if (!req.body.Uid) {
-        return res.status(400).send('El Uid es requerido');
-    }
+    
     userSchema.create(req.body, (error, data) => {
         if (error) {
             return next(error)
