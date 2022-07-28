39d8988759784e739498bf71590f4815c7a12b57
Hector Betancourt <hhbetancourt92@gmail.com>
2022-07-28 09:51:05 -0500

    resrender

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.use((req, res, next) => {
     next(createError(404));
     res.status(404)
     res.send('404 Ruta no encontrada')
+    res.render('404')
 });
 app.use(function (err, req, res, next) {
     console.error(err.message);
