a053e4b2376880cb7dcc310adf0da36d5148ef52
Hector Betancourt <hhbetancourt92@gmail.com>
2022-07-28 10:25:49 -0500

    ch

--- server.js
+++ server.js
@@ -29,16 +29,14 @@ app.listen(app.get("PORT"), () => {
     console.log(`Servidor iniciado en el puerto: ${app.get("PORT")}`);
 });
 // 404 Error
-app.use((err, req, res, next) => {
+app.use((req, res, next) => {
     next(createError(404));
-    if (err.status === 404) {
-        res.send('404 Not Found');
-    }
+    console.log('404 error');
 });
 app.use(function (err, req, res, next) {
     console.error(err.statusCode);
     if (!err.statusCode) err.statusCode = 500;
-    res.status(err.statusCode).send('Error:'+ err.status+' '+err.message);
+    res.status(err.statusCode).send('Error:'+ err.statusCode+' '+err.message);
 });
 app.use((req, res, next)=>{
     res.header("Access-Control-Allow-Origin", "*");
