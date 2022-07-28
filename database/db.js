e599bbe0e16fe692e310adefadabe2de430e7ee4
Hector Betancourt <hhbetancourt92@gmail.com>
2022-07-28 09:26:07 -0500

    db-access

--- database/db.js
+++ database/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    db: 'mongodb+srv://HectorBetan:loUEtIe1PUAARDi1@colombiaemprende.qiittao.mongodb.net/colombia-emprende?retryWrites=true&w=majority',
+    db: 'mongodb+srv://HectorBetan:loUEtIe1PUAARDi1@colombiaemprende.qiittao.mongodb.net/?retryWrites=true&w=majority',
 };
