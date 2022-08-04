e4b1cd7fbf9b5443990119ab52026ed1eed0898b
Hector Betancourt <hhbetancourt92@gmail.com>
2022-08-04 16:00:13 -0500

    get-products

--- routes/public.route.js
+++ routes/public.route.js
@@ -2,8 +2,18 @@ let mongoose = require('mongoose'),
 express = require('express');
 router = express.Router();
 let storeSchema = require('../models/Stores');
+let productSchema = require('../models/Products');
 router.route('/').get((req, res) => {
     storeSchema.find((error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+            res.json(data)
+        }
+    })
+})
+router.route('/products').get((req, res) => {
+    productSchema.find((error, data) => {
         if (error) {
             return next(error)
         } else {
